4d24cb569a18654082b7fe908c5e70b59ea69810
Alexandria201 <aov6lt@ukr.net>
2022-01-26 12:47:15 +0200

    wiget

---
+++ file.js
@@ -0,0 +1,21 @@
+fetch("http://api.weatherapi.com/v1/current.json?key=b677082ac19f45aca07212645220401&q=Kiev&aqi=no")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    render(data)
+  });
+
+function render(data) {
+  document.getElementById("app").innerHTML = `
+    Here is your daily weather!
+    <div> City: ${data.location.name}; </div>
+    <div> Country: ${data.location.country}; </div>
+    <div> Current temperature: ${data.current.temp_c};</div>
+    <div> Humidity:${data.current.humidity}; </div>
+    <div>${data.current.condition.text};</div>
+    <div>
+      <img src="http:////cdn.weatherapi.com/weather/64x64/day/116.png"" alt="">
+    </div>
+    `
+};
