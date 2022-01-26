1750d373cf6377563615b2b07e11dab88b454620
Alexandria201 <aov6lt@ukr.net>
2022-01-26 15:05:48 +0200

    Configuration

--- file.js
+++ file.js
@@ -1,13 +1,12 @@
-fetch("http://api.weatherapi.com/v1/current.json?key=b677082ac19f45aca07212645220401&q=Kiev&aqi=no")
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    render(data)
+fetch('http://api.weatherapi.com/v1/current.json?key=b677082ac19f45aca07212645220401&q=Kiev&aqi=no')
+  .then((response) => response.json())
+  .then((data) => {
+    // eslint-disable-next-line no-use-before-define
+    render(data);
   });
 
 function render(data) {
-  document.getElementById("app").innerHTML = `
+  document.getElementById('app').innerHTML = `
     Here is your daily weather!
     <div> City: ${data.location.name}; </div>
     <div> Country: ${data.location.country}; </div>
@@ -17,5 +16,5 @@ function render(data) {
     <div>
       <img src="http:////cdn.weatherapi.com/weather/64x64/day/116.png"" alt="">
     </div>
-    `
-};
+    `;
+}
